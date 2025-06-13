0030c414ad0441a3ee78b7ab978e263430cf5211
hankil.seo <hankil.seo@hankilseoui-MacBookPro.local>
2025-06-13 15:26:02 +0900

    initial commits

---
+++ minified.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const files = [
+    'argocd-2.14.2.json',
+    'argoworkflow-3.5.4.json',
+    'grafana-11.4.0.json',
+    'gitlab-latest-filtered.json',
+    'jira-latest-filtered.json',
+]
+
+files.forEach(file => {
+    const raw = fs.readFileSync(`original/${file}`, 'utf-8');
+    const minified = JSON.stringify(JSON.parse(raw));
+
+    fs.writeFileSync(`minified/${file}`, minified);
+    console.log(`${file} 줄바꿈 제거 완료`);
+})
+
---
+++ run.sh
@@ -0,0 +1,140 @@
+openapi-filter --checkTags \
+  --flags Issues \
+  "Issue search" \
+  Projects \
+  Users \
+  Workflows \
+  "Issue comments" \
+  "Issue Attachments" \
+  Filters \
+  Dashboards \
+  Groups \
+  "Issue fields" \
+  "Issue types" \
+  "Issue type hierarchy" \
+  "JQL" \
+  "Permissions" \
+  "Project components" \
+  "Project key and name validation" \
+  "Project roles" \
+  "Project types" \
+  "Server info" \
+  "Tasks" \
+  "User properties" \
+  "User search" \
+  "Webhooks" \
+  -- original/jira-latest.json original/jira-latest-filtered.json
+
+openapi-filter --checkTags \
+  --flags award_emoji \
+  badges \
+  runners \
+  group_avatar \
+  invitations \
+  members \
+  personal_access_tokens \
+  wikis \
+  alert_management \
+  branches \
+  secure_files \
+  cargo_packages \
+  commits \
+  pages \
+  pages_domains \
+  project_avatar \
+  projects_job_token_scope \
+  project_snippets \
+  protected_tags \
+  remote_mirrors \
+  tags \
+  batched_background_migrations \
+  admin \
+  migrations \
+  broadcast_messages \
+  applications \
+  avatar \
+  bulk_imports \
+  job \
+  jobs \
+  events \
+  keys \
+  markdown \
+  namespaces \
+  organizations \
+  snippets \
+  hooks \
+  usage_data \
+  metrics \
+  user_counts \
+  user \
+  application \
+  import \
+  slack \
+  topics \
+  web_commits \
+  access_requests \
+  ci_lint \
+  ci_resource_groups \
+  ci_variables \
+  cluster_agents \
+  clusters \
+  composer_packages \
+  conan_packages \
+  container_registry \
+  container_registry_event \
+  debian_distribution \
+  debian_packages \
+  dependency_proxy \
+  deploy_keys \
+  deploy_tokens \
+  deployments \
+  dora_metrics \
+  environments \
+  error_tracking_client_keys \
+  error_tracking_project_settings \
+  feature_flags_user_lists \
+  feature_flags \
+  features \
+  freeze_periods \
+  generic_packages \
+  geo \
+  geo_nodes \
+  go_proxy \
+  group_export \
+  group_import \
+  group_packages \
+  helm_packages \
+  integrations \
+  issue_links \
+  jira_connect_subscriptions \
+  maven_packages \
+  merge_requests \
+  metadata \
+  ml_model_registry \
+  npm_packages \
+  nuget_packages \
+  package_files \
+  plan_limits \
+  project_export \
+  project_hooks \
+  project_import \
+  project_import_bitbucket \
+  project_import_github \
+  project_packages \
+  protected environments \
+  pypi_packages \
+  release_links \
+  releases \
+  resource_milestone_events \
+  rpm_packages \
+  rubygem_packages \
+  suggestions \
+  system_hooks \
+  terraform_state \
+  terraform_registry \
+  unleash_api \
+  -- original/gitlab-latest.json original/gitlab-latest-filtered.json
+
+
+
+
