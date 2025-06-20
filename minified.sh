2d94a8ae573d713baecc0d8661959b27e6582212
hankil.seo <hankil.seo@hankilseoui-MacBookPro.local>
2025-06-20 14:23:16 +0900

    html minified...

--- minified.sh
+++ minified.sh
@@ -2,96 +2,30 @@
 # Dashboards, Groups, Issues, Projects, Workflows, Users
 openapi-filter --checkTags \
   --flags "Announcement banner" \
-  "Filters" \
-  "JQL" \
-  "Issue comments" \
+  "Screen tabs" \
+  "Screens" \
+  "Screen tab fields" \
+  "Screen schemes" \
+  "Service Registry" \
+  "UI modifications (apps)" \
+  "Announcement banner" \
   "App data policies" \
   "App migration" \
-  "App properties" \
-  "Application roles" \
   "Audit records" \
   "Avatars" \
   "Classification levels" \
   "Dynamic modules" \
-  "Filter sharing" \
-  "Group and user picker" \
-  "Issue attachments" \
-  "Issue bulk operations" \
-  "Issue comment properties" \
-  "Issue custom field associations" \
-  "Issue custom field configuration (apps)" \
-  "Issue custom field contexts" \
-  "Issue custom field options" \
-  "Issue custom field options (apps)" \
-  "Issue custom field values (apps)" \
-  "Issue field configurations" \
-  "Issue fields" \
-  "Issue link types" \
-  "Issue links" \
-  "Issue navigator settings" \
-  "Issue notification schemes" \
-  "Issue priorities" \
-  "Issue properties" \
-  "Issue redaction" \
-  "Issue remote links" \
-  "Issue resolutions" \
-  "Issue search" \
-  "Issue security level" \
-  "Issue security scheme" \
-  "Issue security schemes" \
-  "Issue type properties" \
-  "Issue type schemes" \
-  "Issue type screen schemes" \
-  "Issue types" \
-  "Issue votes" \
-  "Issue watchers" \
-  "Issue worklog properties" \
-  "Issue worklogs" \
-  "JQL functions (apps)" \
-  "Jira expressions" \
-  "Jira settings" \
-  "Labels" \
   "License metrics" \
-  "Myself" \
-  "Permission schemes" \
-  "Permissions" \
   "Plans" \
   "Priority schemes" \
-  "Project avatars" \
-  "Project categories" \
-  "Project classification levels" \
-  "Project components" \
-  "Project email" \
-  "Project features" \
-  "Project key and name validation" \
-  "Project permission schemes" \
-  "Project properties" \
-  "Project role actors" \
-  "Project roles" \
-  "Project templates" \
-  "Project types" \
-  "Project versions" \
-  "Screen schemes" \
-  "Screen tab fields" \
-  "Screen tabs" \
-  "Screens" \
-  "Server info" \
-  "Service Registry" \
-  "Status" \
-  "Tasks" \
+  "App properties" \
+  "Application roles" \
+  "usernavproperties" \
   "Teams in plan" \
   "Time tracking" \
-  "UI modifications (apps)" \
-  "User properties" \
-  "User search" \
-  "Workflow scheme drafts" \
-  "Workflow scheme project associations" \
-  "Workflow schemes" \
-  "Workflow status categories" \
-  "Workflow statuses" \
-  "Workflow transition properties" \
-  "Workflow transition rules" \
-  "Webhooks" \
+  "Filter sharing" \
+  "Filters" \
+  "Issue bulk operations" \
   -- original/jira-latest.json original/jira-latest-filtered.json
 
 # gitlab
@@ -99,45 +33,28 @@ openapi-filter --checkTags \
 openapi-filter --checkTags \
   --flags award_emoji \
   badges \
-  runners \
   group_avatar \
   invitations \
-  members \
-  personal_access_tokens \
   wikis \
   alert_management \
-  branches \
   secure_files \
   cargo_packages \
-  commits \
   pages \
   pages_domains \
   project_avatar \
-  projects_job_token_scope \
-  project_snippets \
-  protected_tags \
   remote_mirrors \
-  tags \
   batched_background_migrations \
-  admin \
   migrations \
   broadcast_messages \
-  applications \
   avatar \
   bulk_imports \
-  job \
-  jobs \
-  events \
   keys \
   markdown \
   namespaces \
   organizations \
   snippets \
-  hooks \
   usage_data \
   metrics \
-  user_counts \
-  user \
   application \
   import \
   slack \
@@ -151,14 +68,10 @@ openapi-filter --checkTags \
   clusters \
   composer_packages \
   conan_packages \
-  container_registry \
   container_registry_event \
   debian_distribution \
   debian_packages \
   dependency_proxy \
-  deploy_keys \
-  deploy_tokens \
-  deployments \
   dora_metrics \
   environments \
   error_tracking_client_keys \
@@ -179,7 +92,6 @@ openapi-filter --checkTags \
   issue_links \
   jira_connect_subscriptions \
   maven_packages \
-  merge_requests \
   metadata \
   ml_model_registry \
   npm_packages \
@@ -191,11 +103,9 @@ openapi-filter --checkTags \
   project_import \
   project_import_bitbucket \
   project_import_github \
-  project_packages \
   protected environments \
   pypi_packages \
   release_links \
-  releases \
   resource_milestone_events \
   rpm_packages \
   rubygem_packages \
@@ -212,4 +122,4 @@ redocly bundle original/gitlab-latest-filtered.json --remove-unused-components -
 bash clean.sh original/jira-latest-filtered-redocly.json original/jira-latest-cleaned.json
 bash clean.sh original/gitlab-latest-filtered-redocly.json original/gitlab-latest-cleaned.json
 
-node minified.js
+bash pages.sh
--- pages.sh
+++ pages.sh
@@ -1,11 +1,11 @@
 #redocly build-docs minified/argoworkflow-3.5.4.json --output=pages/argoworkflow-3.5.4-dark.html --config redocly-dark.yaml
 redocly build-docs original/argocd-2.14.2.json --output=pages/argocd-2.14.2-dark.html --config redocly-dark.yaml
-redocly build-docs original/gitlab-latest.json --output=pages/gitlab-latest-dark.html --config redocly-dark.yaml
-redocly build-docs original/jira-latest.json --output=pages/jira-latest-dark.html --config redocly-dark.yaml
+redocly build-docs original/gitlab-latest-cleaned.json --output=pages/gitlab-latest-dark.html --config redocly-dark.yaml
+redocly build-docs original/jira-latest-cleaned.json --output=pages/jira-latest-dark.html --config redocly-dark.yaml
 redocly build-docs original/grafana-11.4.0.json --output=pages/grafana-11.4.0-dark.html --config redocly-dark.yaml
 
 #redocly build-docs minified/argoworkflow-3.5.4.json --output=pages/argoworkflow-3.5.4-light.html --config redocly-light.yaml
 redocly build-docs original/argocd-2.14.2.json --output=pages/argocd-2.14.2-light.html --config redocly-light.yaml
-redocly build-docs original/gitlab-latest.json --output=pages/gitlab-latest-light.html --config redocly-light.yaml
-redocly build-docs original/jira-latest.json --output=pages/jira-latest-light.html --config redocly-light.yaml
+redocly build-docs original/gitlab-latest-cleaned.json --output=pages/gitlab-latest-light.html --config redocly-light.yaml
+redocly build-docs original/jira-latest-cleaned.json --output=pages/jira-latest-light.html --config redocly-light.yaml
 redocly build-docs original/grafana-11.4.0.json --output=pages/grafana-11.4.0-light.html --config redocly-light.yaml
